8aff2573097e88f032c304a10054ee5270586128
capt-farvez <farvez.capt@gmail.com>
2025-05-24 03:16:58 +0600

    Refactor Docker setup: streamline Dockerfile, update entrypoint script, and clean up docker-compose.yml

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,5 +4,4 @@ source .venv/bin/activate
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
-
-exec gunicorn --timeout 600 --bind 0.0.0.0:8000 testproject.wsgi:application --access-logfile - --error-logfile - --log-level debug
+python manage.py runserver
